6f5dbf9ce49cfe1fe03c263fe6e8831bb9ecd249
fecalci <68797728+fecalci@users.noreply.github.com>
2021-05-10 16:55:42 -0300

    Update created Transacciones

--- PersonalAccountManager/src/main/java/com/sistemasactivos/pam/models/repository/TransaccionesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sistemasactivos.pam.models.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.sistemasactivos.pam.models.entity.Transacciones;
-
-public interface TransaccionesRepository extends CrudRepository <Transacciones, Integer> {
-
-}
